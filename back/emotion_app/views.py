bdc2dd4a04a22e352ee14151c28e77786aafede1
Fratu <143589461+syarasyoujyu@users.noreply.github.com>
2024-09-21 21:15:14 +0900

    感情分析

--- back/emotion_app/views.py
+++ back/emotion_app/views.py
@@ -3,7 +3,82 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import whisper
 from transformers import pipeline
+# Importing other libraries
+import joblib
+import numpy as np
+import sys
+import os
 
+# Audio Imports
+import pyaudio
+import wave
+import librosa
+# Opening Model
+MLP = joblib.load("back/emotion_app/assets/MLP.joblib")
+
+# Audio Capture Parameters
+CHUNKSIZE = 1024
+RATE = 44100
+p = pyaudio.PyAudio()
+def start_stream(index=1):
+    """Initializing PyAudio Capture Stream"""
+    global stream
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE,
+                    input=True, frames_per_buffer=CHUNKSIZE,
+                    input_device_index=index)
+def stop_stream():
+    """Terminate stream."""
+    stream.stop_stream()
+    stream.close()
+def get_emotion(filename:str):
+    """Predict emotion."""
+    global stream
+    start_stream()
+    frames = []
+    for _ in range(0, int(RATE / CHUNKSIZE * 1)):
+        data = stream.read(CHUNKSIZE)
+        frames.append(data)
+    with wave.open(filename, 'wb') as file:
+        file.setnchannels(1)
+        file.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+        file.setframerate(RATE)
+        file.writeframes(b''.join(frames))
+    features = get_features(filename)
+    emotion=MLP.predict(features)[0]
+    stop_stream()
+    p.terminate()
+    return emotion
+def get_features(filename: str):
+    """
+    Extract features from audio required for model training.
+
+    return in required format.
+    """
+    # Reading the file in and extracting required data
+    raw_data, sr = librosa.load(filename)
+
+    # Creating an empty numpy array to add data later on
+    data = np.array([])
+
+    # Calculating & Appending mfcc
+    mfcc = np.mean(librosa.feature.mfcc(
+        y=raw_data, sr=sr, n_mfcc=40).T, axis=0)
+    data = np.hstack((data, mfcc))
+
+    # Calculating & Appending chroma
+    stft = np.abs(librosa.stft(raw_data))
+    chroma = np.mean(librosa.feature.chroma_stft(
+        S=stft, sr=sr,).T, axis=0)
+    data = np.hstack((data, chroma))
+
+    # Calculating & Appending mel
+    mel = np.mean(librosa.feature.melspectrogram(
+        y=raw_data, sr=sr).T,axis=0)
+    data = np.hstack((data, mel))
+
+    x = []
+    x.append(data)
+    return np.array(x)
 # Whisperモデルのロード
 model = whisper.load_model('base')  # 'tiny', 'small', 'medium', 'large'から選択可能
 
@@ -12,7 +87,6 @@ sentiment_model = pipeline('sentiment-analysis', model='daigo/bert-base-japanese
 @csrf_exempt
 def transcribe_audio(request):
     if request.method == 'POST':
-        if request.method == 'POST':
         audio_file = request.FILES.get('audio')
         if not audio_file:
             return JsonResponse({'error': '音声ファイルが提供されていません。'}, status=400)
@@ -29,16 +103,16 @@ def transcribe_audio(request):
             text = result.get('text')
         except Exception as e:
             return JsonResponse({'error': f'音声認識中にエラーが発生しました: {str(e)}'}, status=500)
+
+        # 感情分析の実行
+        try:
+            emotion = get_emotion(temp_file_path)
+        except Exception as e:
+            return JsonResponse({'error': f'感情分析中にエラーが発生しました: {str(e)}'}, status=500)
         finally:
             # 一時ファイルの削除
             temp_file.close()
 
-        # 感情分析の実行
-        try:
-            sentiment = sentiment_model(text)
-        except Exception as e:
-            return JsonResponse({'error': f'感情分析中にエラーが発生しました: {str(e)}'}, status=500)
-
-        return JsonResponse({'text': text, 'sentiment': sentiment})
+        return JsonResponse({'text': text, 'emotion': emotion})
     else:
         return JsonResponse({'error': '無効なリクエストメソッドです。'}, status=405)
